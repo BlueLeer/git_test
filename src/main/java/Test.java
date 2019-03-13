73f0faff52728570ad6edf2c77d7ca8b96f3e803
wangle <251668577@qq.com>
2019-03-13 09:49:44 +0800

    添加pom和修改gitignore

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author WangLe
+ * @date 2019/3/13 9:49
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}

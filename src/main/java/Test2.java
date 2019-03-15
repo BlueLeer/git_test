4bbe0a7c970a3d521bb7d0cda3812512f528e1f4
wangle <251668577@qq.com>
2019-03-15 16:12:12 +0800

    添加Test2

---
+++ src/main/java/Test2.java
@@ -0,0 +1,10 @@
+/**
+ * @author WangLe
+ * @date 2019/3/15 16:04
+ * @description
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("秦臻666");
+    }
+}
